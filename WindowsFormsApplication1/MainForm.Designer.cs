ed1cc51b2aa79ab66e6d4be294bd25fcee5cda57
Ryan L. Boyd <ryanb@ryanb.cc>
2018-05-19 11:02:50 -0500

    v0.65
    Initial Commit

---
+++ WindowsFormsApplication1/MainForm.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace ZhTokenApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.BgWorker = new System.ComponentModel.BackgroundWorker();
+            this.ScanSubfolderCheckbox = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.FolderBrowser = new System.Windows.Forms.FolderBrowserDialog();
+            this.FilenameLabel = new System.Windows.Forms.Label();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.EncodingDropdown = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BgWorker
+            // 
+            this.BgWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BgWorker_DoWork);
+            this.BgWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BgWorker_RunWorkerCompleted);
+            // 
+            // ScanSubfolderCheckbox
+            // 
+            this.ScanSubfolderCheckbox.AutoSize = true;
+            this.ScanSubfolderCheckbox.Location = new System.Drawing.Point(253, 60);
+            this.ScanSubfolderCheckbox.Name = "ScanSubfolderCheckbox";
+            this.ScanSubfolderCheckbox.Size = new System.Drawing.Size(108, 17);
+            this.ScanSubfolderCheckbox.TabIndex = 2;
+            this.ScanSubfolderCheckbox.Text = "Scan subfolders?";
+            this.ScanSubfolderCheckbox.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(233, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 32);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FolderBrowser
+            // 
+            this.FolderBrowser.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            // 
+            // FilenameLabel
+            // 
+            this.FilenameLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.FilenameLabel.Location = new System.Drawing.Point(16, 97);
+            this.FilenameLabel.Name = "FilenameLabel";
+            this.FilenameLabel.Size = new System.Drawing.Size(373, 15);
+            this.FilenameLabel.TabIndex = 6;
+            this.FilenameLabel.Text = "Waiting to tokenize your texts...";
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.FileName = "Repeatalizer.csv";
+            this.saveFileDialog.Filter = "CSV Files|*.csv";
+            this.saveFileDialog.Title = "Please choose where to save your output";
+            // 
+            // EncodingDropdown
+            // 
+            this.EncodingDropdown.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.EncodingDropdown.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EncodingDropdown.FormattingEnabled = true;
+            this.EncodingDropdown.Location = new System.Drawing.Point(16, 45);
+            this.EncodingDropdown.Name = "EncodingDropdown";
+            this.EncodingDropdown.Size = new System.Drawing.Size(180, 28);
+            this.EncodingDropdown.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(114, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Text Encoding:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.ClientSize = new System.Drawing.Size(401, 121);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.EncodingDropdown);
+            this.Controls.Add(this.FilenameLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ScanSubfolderCheckbox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(417, 160);
+            this.MinimumSize = new System.Drawing.Size(417, 160);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ZhToken";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.ComponentModel.BackgroundWorker BgWorker;
+        private System.Windows.Forms.CheckBox ScanSubfolderCheckbox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog FolderBrowser;
+        private System.Windows.Forms.Label FilenameLabel;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.ComboBox EncodingDropdown;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ WindowsFormsApplication1/MainForm.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+using edu.stanford.nlp.ie.crf;
+using java.util;
+
+
+namespace ZhTokenApplication
+{
+
+    public partial class Form1 : Form
+    {
+
+
+        //this is what runs at initialization
+        public Form1()
+        {
+
+            InitializeComponent();
+
+            foreach(var encoding in Encoding.GetEncodings())
+            {
+                EncodingDropdown.Items.Add(encoding.Name);
+            }
+            try
+            {
+                EncodingDropdown.SelectedIndex = EncodingDropdown.FindStringExact("utf-8");
+            }
+            catch
+            {
+                EncodingDropdown.SelectedIndex = EncodingDropdown.FindStringExact(Encoding.Default.BodyName);
+            }
+
+            
+
+
+        }
+
+
+
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            
+
+            
+
+            FolderBrowser.Description = "Please choose the location of your .txt files";
+            FolderBrowser.ShowDialog();
+            string TextFileFolder = FolderBrowser.SelectedPath.ToString();
+
+            if (TextFileFolder != "")
+            {
+
+                FolderBrowser.Description = "Please choose your output location";                
+
+                if (FolderBrowser.ShowDialog() != DialogResult.Cancel)
+                {
+                    string OutputFolder = FolderBrowser.SelectedPath.ToString();
+
+                    if (TextFileFolder != OutputFolder) { 
+
+                        button1.Enabled = false;
+                        ScanSubfolderCheckbox.Enabled = false;
+                        EncodingDropdown.Enabled = false;
+                        BgWorker.RunWorkerAsync(new string[] { TextFileFolder, OutputFolder });
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Your input folder cannot be the same as your output folder.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+
+                }
+
+            } 
+
+        }
+
+        
+
+
+
+
+        private void BgWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+
+            
+            //selects the text encoding based on user selection
+            Encoding SelectedEncoding = null;
+            this.Invoke((MethodInvoker)delegate ()
+            {
+                SelectedEncoding = Encoding.GetEncoding(EncodingDropdown.SelectedItem.ToString());
+
+            });
+
+
+            //report what we're working on
+            FilenameLabel.Invoke((MethodInvoker)delegate
+            {
+                FilenameLabel.Text = "Loading model... please wait...";
+            });
+
+            // Path to the folder with models
+            var segmenterData = Path.Combine(Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory), @"data");
+
+            var props = new Properties();
+            props.setProperty("sighanCorporaDict", segmenterData);
+            props.setProperty("serDictionary", segmenterData + @"\dict-chris6.ser.gz");
+            // Lines below are needed because CTBSegDocumentIteratorFactory accesses it
+            props.setProperty("inputEncoding", SelectedEncoding.ToString());
+            props.setProperty("sighanPostProcessing", "true");
+
+            var segmenter = new CRFClassifier(props);
+            segmenter.loadClassifierNoExceptions(segmenterData + @"\ctb.gz", props);
+
+
+            //get the list of files
+            var SearchDepth = SearchOption.TopDirectoryOnly;
+            if (ScanSubfolderCheckbox.Checked)
+            {
+                SearchDepth = SearchOption.AllDirectories;
+            }
+            var files = Directory.EnumerateFiles(((string[])e.Argument)[0], "*.txt", SearchDepth);
+
+
+
+            try {
+
+                string outputdir = Path.Combine(((string[])e.Argument)[1]);
+
+                Directory.CreateDirectory(outputdir);
+                
+
+
+                foreach (string fileName in files)
+                {
+
+
+
+                    //set up our variables to report
+                    string Filename_Clean = Path.GetFileName(fileName);
+                    
+
+
+                    //report what we're working on
+                    FilenameLabel.Invoke((MethodInvoker)delegate
+                    {
+                        FilenameLabel.Text = "Analyzing: " + Filename_Clean;
+                    });
+
+                    
+                                            
+
+                    //do stuff here
+                    string readText = File.ReadAllText(fileName, SelectedEncoding).ToLower();
+
+                    string TokenResults = segmenter.classifyToString(readText);
+
+
+                    using (System.IO.StreamWriter fileout =
+                        new StreamWriter(Path.Combine(outputdir, Filename_Clean), false, SelectedEncoding))
+                    {
+                        fileout.Write(TokenResults);
+                    }
+
+                }
+
+
+
+           }
+           catch
+           {
+                MessageBox.Show("ZhToken encountered a problem while trying to tokenize/write a file.");
+           }
+
+
+
+            
+        }
+
+        private void BgWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            button1.Enabled = true;
+            ScanSubfolderCheckbox.Enabled = true;
+            EncodingDropdown.Enabled = true;
+            FilenameLabel.Text = "Finished!";
+            MessageBox.Show("ZhToken has finished segmenting your texts.", "Analysis Complete", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void WordWindowSizeTextbox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void PhraseLengthTextbox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void BigWordTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+    }
+    
+
+
+}
